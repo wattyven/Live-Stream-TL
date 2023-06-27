55341043e71edad6f7512a383882d63d6d65d88a
Watty <66798673+wattyven@users.noreply.github.com>
2023-06-27 11:37:50 -0700

    Update main.py
    improved ease-of-use re: language support

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime
 
+# change this to the language you want to translate from
+# use the two letter code, e.g. "en" for English, "es" for Spanish, etc.
+lang_sel = "cn"
+
 def callback(indata, frames, time, status):
-    global translation_complete
+    global translation_complete, lang
     if status:
         print(status)
     else:
@@ -20,9 +24,9 @@ def callback(indata, frames, time, status):
             result = json.loads(rec.Result())
             if result['text'].strip(): # only print if there is text
                 # was originally messing up the first transcription of every session
-                print("Japanese:", result['text'])
+                print(f"{lang}:", result['text'])
                 # log it too
-                f.write(f"Japanese: {result['text']}\n")  # Modified this line
+                f.write(f"{lang}: {result['text']}\n")  # Modified this line
                 try:
                     # gpt3.5-turbo translation, if you have gpt-4 api access it'd be better
                     response = openai.ChatCompletion.create(
@@ -30,15 +34,15 @@ def callback(indata, frames, time, status):
                         # alternatively, you don't even have to use an openai model, 
                         # you can use a diff service like DeepL or something
                         messages=[
-                            {"role": "system", "content": "You are a helpful assistant."},
-                            {"role": "user", "content": f"Translate the following Japanese sentence to English: {result['text']}"},
+                            {"role": "system", "content": "You are a professional translator. You will reply with nothing but the best translations, and absolutely nothing else."},
+                            {"role": "user", "content": f"Translate the following sentence from {lang} to English: {result['text']}"},
                         ],
                     )
                     translated_text = response['choices'][0]['message']['content']
                 except:
                     translated_text = "Translation failed. Apologies for the inconvenience." # just in case
-                print("English:", translated_text)
-                f.write(f"English: {translated_text}\n")  # Modified this line
+                print("EN:", translated_text)
+                f.write(f"EN: {translated_text}\n")  # Modified this line
                 translation_complete = True # set to true when translation is complete
 
 # open or make a transcription and translation log file
@@ -51,11 +55,21 @@ if __name__ == "__main__":
     load_dotenv()
     openai.api_key = os.getenv('OPENAI_API_KEY')
 
-    # initialize Vosk with Japanese model (full size)
+    # initialize audio stream
+    stream = sd.InputStream(
+        callback=callback, 
+        channels=1, 
+        samplerate=16000, 
+        blocksize=65536)
+
+    # initialize Vosk with model (full size)
     # if you have a weaker machine, i recommend the small model
     # it's faster but only slightly less accurate
-    # model = Model("vosk-model-small-ja-0.22")
-    model = Model("vosk-model-ja-0.22")
+    # model = Model("vosk-model-small-{lang}-0.22")
+    # these models will have to be downloaded if you don't have them already
+    lang = lang_sel.upper()
+    print(f"Language: {lang}")
+    model = Model(f"vosk-model-{lang}-0.22")
 
     # recognizer, 16k hz sample rate to match the vosk model
     rec = KaldiRecognizer(model, 16000)
@@ -71,6 +85,6 @@ if __name__ == "__main__":
         # by default, sd.InputStream uses the default input device
         # I have this set to Stereo Mix. You can use a virtual audio cable as well, 
         # or even a physical cable if you have the right hardware
-        with sd.InputStream(callback=callback, channels=1, samplerate=16000, blocksize=65536):
+        with stream:
             while True:
                 pass
